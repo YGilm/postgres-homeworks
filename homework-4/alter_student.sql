bd902c58c2b91f14e307ec06fa16bb1285a17473
ygilm <ygilm@bk.ru>
2023-07-27 06:13:06 +0400

    added requests to file alter_student.sql

--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -1,19 +1,29 @@
 -- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
-
+CREATE TABLE student
+(
+	student_id SERIAL,
+	first_name VARCHAR,
+	last_name VARCHAR,
+	birthday DATE,
+	phone VARCHAR
+);
 
 -- 2. Добавить в таблицу student колонку middle_name varchar
-
+ALTER TABLE student ADD COLUMN middle_name VARCHAR;
 
 -- 3. Удалить колонку middle_name
-
+ALTER TABLE student DROP COLUMN middle_name;
 
 -- 4. Переименовать колонку birthday в birth_date
-
+ALTER TABLE student RENAME COLUMN birthday TO birth_date;
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
-
+ALTER TABLE student ALTER COLUMN phone SET DATA TYPE VARCHAR(32);
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
-
+INSERT INTO student (first_name, last_name, birth_date, phone) VALUES ('Yury', 'Gilm', '1986-11-26', 7779977);
+INSERT INTO student (first_name, last_name, birth_date, phone) VALUES ('Vasily', 'Shmidt', '1989-10-16', 3339947);
+INSERT INTO student (first_name, last_name, birth_date, phone) VALUES ('Jane', 'Smith', '1993-07-05', 4239097);
 
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
+TRUNCATE TABLE student RESTART IDENTITY;
